0993e4430c9c7a7eb5bf44d00f63d1f6560f6a3a
natb1899 <natal.bumann@students.hevs.ch>
2023-10-08 15:48:47 +0200

    changes config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: ({ versionDocsDirPath, docPath }) =>
-            `https://github.com/${organizationName}/${projectName}/tree/main/`,
+            `https://github.com/${organizationName}/${projectName}/tree/main/${versionDocsDirPath}/${docPath}`,
         },
         blog: {
           showReadingTime: true,
